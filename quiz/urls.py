9f60fdcd7500482f57168feccac3c5f20b36b2f3
Azar <azarsha69@gmail.com>
2021-01-11 05:30:44 -0800

    commit

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     
     path('', include('accounts.urls')),
     path('', include('question.urls')),
     path('admin/', admin.site.urls),
-   
-   
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
 ]
